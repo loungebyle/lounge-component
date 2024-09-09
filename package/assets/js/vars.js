e7e34fceb46c5845c88f9869a964793a7f643553
lé <elcasaboxi@gmail.com>
2024-09-09 16:14:15 +0700

    vars twix

--- package/assets/js/vars.js
+++ package/assets/js/vars.js
@@ -1,12 +1,12 @@
 export const API_KEY = `component`;
-export const FALLBACK_USER_IMAGE = `https://your-website.name/images/fallback/user_image.png`;
+export const FALLBACK_USER_IMAGE = `https://lounge.so/images/fallback/user_image.png`;
 
 // prod
 
-// export const API_ENDPOINT = `https://your-api.herokuapp.com/`;
-// export const URL = `https://your-website.name`;
+// export const API_ENDPOINT = `https://lounge-api.herokuapp.com/`;
+// export const URL = `https://lounge.so`;
 
 // dev
 
-export const API_ENDPOINT = `http://localhost:3001/`;
+export const API_ENDPOINT = `http://localhost:3017/`;
 export const URL = `http://localhost:3000`;
