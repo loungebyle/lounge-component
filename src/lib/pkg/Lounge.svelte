cd962ddcfda9a1f28c6eb4e7d8693924e0501826
lé <elcasaboxi@gmail.com>
2024-09-10 12:25:31 +0700

    panel tba

--- src/lib/pkg/Lounge.svelte
+++ src/lib/pkg/Lounge.svelte
@@ -23,11 +23,75 @@
 	let user;
 	let project;
 	let room_id = ``;
-	let staff_projects = [];
+	let staffed_projects = [];
 	let bookmarked_projects = [];
 	let explorable_projects = [];
 
 	let is_toggled = false;
+	let view = `main`; // <`main`, `avatar`, `account`, `projects`, `users`, `friends`, `rooms`, `project_settings`, `shop`, `help`, `lounge_settings`>
+	let overlay_user;
+	let overlay_project;
+
+	// vars (main)
+	// tba
+
+	// vars (avatar)
+
+	const AVATAR_PROFILE_TABS = [
+		{
+			code: `profile`,
+			label: `Project avatar profile`
+		},
+		{
+			code: `default`,
+			label: `Global avatar profile`
+		}
+	];
+
+	const AVATAR_PART_TABS = [
+		{
+			code: `preview`,
+			name: `Preview`
+		},
+		{
+			code: `body`,
+			name: `Body`
+		},
+		{
+			code: `pet`,
+			name: `Pet`
+		}
+	];
+	
+	let avatar_profile_tab = ``; // based on AVATAR_PROFILE_TABS
+	let avatar_part_tab = ``; // based on AVATAR_PART_TABS
+
+	// vars (account)
+	// tba
+
+	// vars (projects)
+	// tba
+
+	// vars (users)
+	// tba
+
+	// vars (friends)
+	// tba
+
+	// vars (rooms)
+	// tba
+
+	// vars (project_settings)
+	// tba
+
+	// vars (shop)
+	// tba
+
+	// vars (help)
+	// tba
+
+	// vars (lounge_settings)
+	// tba
 
 	// dynamics
 	// none
@@ -81,7 +145,11 @@
 				}) || null;
 
 				if (data) {
-					// todo: data
+					project = data.project || null;
+					room_id = data.room_id || ``;
+					staffed_projects = data.staffed_projects || [];
+					bookmarked_projects = data.bookmarked_projects || [];
+					explorable_projects = data.explorable_projects || [];
 				}
 			}
 
@@ -119,10 +187,140 @@
 
 		<!-- panel -->
 		<div class="container  row--  row-centre--  text  text-white--  card  black--  panel">
+			<!-- label -->
+			<a
+				href="https://lounge.so"
+				target="_blank"
+				rel="noreferrer"
+				class="container  row--  row-centre--  text-white--  card  black--  p-label"
+			>
+				<span>Powered by</span>
+				<span>Lounge.so</span>
+				<span>by Lefrost</span>
+			</a>
+
+			{#if
+				(overlay_user && overlay_user.id) ||
+				(overlay_project && overlay_project.id)	
+			}
+				<!-- overlay container -->
+				<div class="container  grow--  stretch--  col--  col-bottom--  card  black--  p-overlay-container">
+					<!-- overlay -->
+					<div class="container  stretch--  col--  text  text-white--  card  black--  p-overlay">
+						<!-- overlay -> top -->
+						<!-- note: use `p-top` styles -->
+						<div class="container  stretch--  row--  row-left--  p-top">
+							<!-- overlay -> top -> text -->
+							<div class="container  grow--  row--  row-left--  p-to__text">
+								<span>Esc</span>
+								<span>to close</span>
+							</div>
+
+							<!-- top -> close -->
+							<div
+								class="p-to__close"
+								on:click={() => {
+									try {
+										overlay_user = null;
+										overlay_project = null;
+									} catch (e) {
+										console.log(e);
+									}
+								}}
+							>
+								<img
+									src="/images/icons/close.svg"
+									alt=""
+									class="svg  svg-white--"
+								/>
+							</div>
+						</div>
+
+						{#if overlay_user && overlay_user.id}
+							<!-- overlay (user) -> [row] (1) -->
+							<!-- note: use `p-row` styles -->
+							<div class="container  stretch--  row--  row-left--  p-row">
+								<!-- overlay (user) -> row (1) -> avatar -->
+								<!-- tba -->
+
+								<!-- overlay (user) -> row (1) -> user -->
+								<div class="container  grow--  stretch--  col--  text  text-cream--  card  cream--  p-ov__user">
+									<div class="text  text-white--">{overlay_user.name || `n/a`}</div>
+									<div>@{overlay_user.code || `n/a`}</div>
+								</div>
+							</div>
+
+							<!-- overlay (user) -> [row] (2) -->
+							<!-- note: use `p-row` styles -->
+							<div class="container  stretch--  row--  row-left--  p-row">
+								<!-- overlay (user) -> row (2) -> friend -->
+								<!-- tba -->
+
+								{#if false}
+									<!-- overlay (user) -> row (2) -> remove -->
+									<!-- tba -->
+								{/if}
+							</div>
+
+							<!-- overlay (user) -> [row] (3) -->
+							<!-- note: use `p-row` styles -->
+							<div class="container  stretch--  row--  row-left--  p-row">
+								<!-- overlay (user) -> row (3) -> cxs -->
+								<!-- tba -->
+
+								<!-- overlay (user) -> row (3) -> xp -->
+								<!-- tba -->
+							</div>
+						{:else if overlay_project && overlay_project.id}
+							<!-- overlay (project) -> [row] (1) -->
+							<!-- note: use `p-row` styles -->
+							<div class="container  stretch--  row--  row-left--  p-row">
+								<!-- overlay (project) -> row (1) -> avatar -->
+								<!-- tba -->
+
+								<!-- overlay (project) -> row (1) -> project -->
+								<!-- tba -->
+							</div>
+
+							<!-- overlay (project) -> [row] (2) -->
+							<!-- note: use `p-row` styles -->
+							<div class="container  stretch--   row--  row-left--  p-row">
+								<!-- overlay (project) -> row (2) -> users -->
+								<!-- tba -->
+
+								<!-- overlay (project) -> row (2) -> rooms -->
+								<!-- tba -->
+							</div>
+
+							<!-- overlay (project) -> [row] (3) -->
+							<!-- note: use `p-row` styles -->
+							<div class="container  stretch--  row--  row-left--  p-row">
+								<!-- overlay (project) -> row (3) -> cxs -->
+								<!-- tba -->
+
+								<!-- overlay (project) -> row (3) -> xp -->
+								<!-- tba -->
+							</div>
+						{/if}
+					</div>
+				</div>
+			{/if}
+
 			<!-- top -->
 			<div class="container  stretch--  row--  row-left--  p-top">
 				<!-- top -> text -->
-				<!-- tba -->
+				<div class="container  grow--  row--  row-left--  p-to__text">
+					{#if !(user && user.id)}
+						<span>Login to the</span>
+						<span>Lounge.</span>
+					{:else if view === `main`}
+						<span>Welcome to the</span>
+						<span>Lounge.</span>
+					{:else}
+						<span>Esc</span>
+						<span>to go back</span>
+					{/if}
+				</div>
 	
 				<!-- top -> close -->
 				<div
@@ -145,12 +343,97 @@
 	
 			{#if !(user && user.id)}
 				<!-- tba: logged-out view -->
-			{:else if
-				data &&
-				project &&
-				project.id	
-			}
-				<!-- tba: full view -->
+			{:else if !(project && project.id)}
+				<!-- tba: error -->
+			{:else if view === `main`}
+				<!-- panel -> main -->
+				<div class="container  stretch--  col--  p-main">
+					<!-- panel -> main -> [row] (1) -->
+					<!-- tba -->
+
+					<!-- panel -> main -> [row] (2) -->
+					<!-- tba -->
+
+					<!-- panel -> main -> [row] (3) -->
+					<!-- tba -->
+
+					<!-- panel -> main -> [row] (4) -->
+					<!-- tba -->
+
+					<!-- panel -> main -> [row] (5) -->
+					<!-- tba -->
+				</div>
+			{:else if view === `avatar`}
+				<!-- panel -> avatar -->
+				<div class="container  stretch--  col--  p-avatar">
+					<!-- panel -> avatar -> [heading] -->
+					<!-- tba -->
+
+					<!-- panel -> avatar -> profiles -->
+					<!-- tba -->
+
+					<!-- panel -> avatar -> [tabs] -->
+					<!-- tba -->
+
+					{#if avatar_part_tab === `preview`}
+						<!-- tba -->
+					{:else if avatar_part_tab === `body`}
+						<!-- tba -->
+					{:else if avatar_part_tab === `pet`}
+						<!-- tba -->
+					{/if}
+				</div>
+			{:else if view === `account`}
+				<!-- panel -> account -->
+				<div class="container  stretch--  col--  p-account">
+					<!-- panel -> account -> [heading] -->
+					<!-- tba -->
+
+					<!-- panel -> account -> inputs -->
+					<div class="container  stretch--  col--  p-ac__inputs">
+						<!-- panel -> account -> [input] (name) -->
+						<!-- tba -->
+
+						<!-- panel -> account -> [input] (code) -->
+						<!-- tba -->
+
+						<!-- panel -> account -> [input] (link url) -->
+						<!-- todo -->
+
+						<!-- panel -> account -> connection (google) -->
+						<!-- tba -->
+
+						<!-- panel -> account -> connection (discord) -->
+						<!-- tba -->
+
+						<!-- panel -> account -> connection (solana) -->
+						<!-- tba -->
+					</div>
+				</div>
+			{:else if view === `projects`}
+				<!-- panel -> projects -->
+				<!-- tba -->
+			{:else if view === `users`}
+				<!-- panel -> users -->
+				<!-- tba -->
+			{:else if view === `friends`}
+				<!-- panel -> friends -->
+				<!-- tba -->
+			{:else if view === `rooms`}
+				<!-- panel -> rooms -->
+				<!-- tba -->
+			{:else if view === `project_settings`}
+				<!-- panel -> psettings -->
+				<!-- tba -->
+			{:else if view === `shop`}
+				<!-- panel -> shop -->
+				<!-- tba -->
+			{:else if view === `help`}
+				<!-- panel -> help -->
+				<!-- tba -->
+			{:else if view === `lounge_settings`}
+				<!-- panel -> lsettings -->
+				<!-- tba -->
 			{/if}
 		</div>
 	</div>
